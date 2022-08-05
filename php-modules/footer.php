7af71cd3086a0f06d8ea26d4748a764cb94e73b3
kalebkloppe <kaleb@fractalnetworks.co>
2022-08-05 14:19:48 -0500

    update analytics ID

--- php-modules/footer.php
+++ php-modules/footer.php
@@ -1,10 +1,9 @@
 <!--Google Analytics-->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119373378-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CDY4ZEZB46"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-119373378-1');
+
+  gtag('config', 'G-CDY4ZEZB46');
 </script>
